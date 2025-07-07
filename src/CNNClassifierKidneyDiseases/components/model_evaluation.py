ee6ec0b608e672e5ffa25ab81981011a7e96a07c
Khadrawy <omarkhadrawy10@gmail.com>
2025-07-07 19:50:58 +0300

    DVC Pipelines

--- src/CNNClassifierKidneyDiseases/components/model_evaluation.py
+++ src/CNNClassifierKidneyDiseases/components/model_evaluation.py
@@ -79,7 +79,7 @@ class Evaluation:
         mlflow=True
     )
         mlflow.set_registry_uri(self.config.mlflow_uri)
-        #mlflow.set_tracking_uri(self.config.mlflow_uri)
+        mlflow.set_tracking_uri(self.config.mlflow_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
